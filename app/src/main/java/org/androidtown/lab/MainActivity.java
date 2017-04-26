f157223fe7ad39caded825754b6909a21c4cc45f
j096 <ji096@hanmail.net>
2017-04-26 15:56:01 +0900

    Lab04

--- app/src/main/java/org/androidtown/lab/MainActivity.java
+++ app/src/main/java/org/androidtown/lab/MainActivity.java
@@ -1,26 +1,32 @@
 package org.androidtown.lab;
 
-import android.app.SearchManager;
+
 import android.content.Context;
-import android.net.Uri;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.ContextMenu;
+import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.LinearLayout;
-import android.content.Intent;
-import android.widget.Toast;
-
-
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
 
 
 public class MainActivity extends AppCompatActivity {
 
 
-    EditText Age;
-    EditText Name;
     Button button1;
+    EditText ed;
+    RadioGroup radGroupMW;
+    RadioButton M;
+    RadioButton W;
+    CheckBox cb1;
+    CheckBox cb2;
 
 
     @Override
@@ -28,30 +34,46 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
-        Name=(EditText)findViewById(R.id.editText1);
-        Age=(EditText)findViewById(R.id.editText2);
         button1=(Button)findViewById(R.id.button1);
+        ed=(EditText)findViewById(R.id.nameline);
+        radGroupMW=(RadioGroup)findViewById(R.id.radGroupMW);
+        M=(RadioButton) findViewById(R.id.M);
+        W=(RadioButton)findViewById(R.id.W);
+        cb1=(CheckBox)findViewById(R.id.sms);
+        cb2=(CheckBox)findViewById(R.id.email);
+
 
         button1.setOnClickListener(new View.OnClickListener(){
-
             @Override
             public void onClick(View view) {
-                String n=Name.getText().toString();
-                String a=Age.getText().toString();
-                Intent intent=new Intent(getApplicationContext(),NewActivity.class);
-                intent.putExtra("name",n);
-                intent.putExtra("age",a);
-                startActivity(intent);
+                Intent i=new Intent(getApplicationContext(),NewActivity.class);
+
+                String name=ed.getText().toString();
+                String check = ":";
+                int radioId=radGroupMW.getCheckedRadioButtonId();
+
+                if(M.getId()==radioId)
+                    i.putExtra("MW","남");
+
+                if(W.getId()==radioId)
+                    i.putExtra("MW","여");
+
+                if(cb1.isChecked()) {
+                    check += "SMS";
+                    i.putExtra("check", check);
+                }
+                if(cb2.isChecked()) {
+                    check+="e-mail";
+                    i.putExtra("check", check);
+                }
+
+                i.putExtra("name",name);
+                startActivity(i);
+
             }
         });
-
-
-
-
-
-
     }
+
 
 
 }
--- app/src/main/java/org/androidtown/lab/NewActivity.java
+++ app/src/main/java/org/androidtown/lab/NewActivity.java
@@ -5,6 +5,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import static android.R.attr.data;
@@ -12,26 +13,36 @@ import static android.R.attr.data;
 public class NewActivity extends AppCompatActivity {
 
 
+    TextView tv1;
+    TextView tv2;
+    TextView tv3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new);
 
+        tv1=(TextView)findViewById(R.id.nameline);
+        tv2=(TextView)findViewById(R.id.mwline);
+        tv3=(TextView)findViewById(R.id.sendline);
+
+
         Intent passedIntent = getIntent();
 
         if(passedIntent!=null){
 
 
-            String outName=passedIntent.getStringExtra("name");
-            String outAge=passedIntent.getStringExtra("age");
+            String name=passedIntent.getStringExtra("name");
+            String MW=passedIntent.getStringExtra("MW");
+            String send=passedIntent.getStringExtra("check");
 
-            Toast.makeText(getApplicationContext(),"Student Info "+outName+" "+outAge,Toast.LENGTH_LONG).show();
-
-
+            tv1.setText(":"+name);
+            tv2.setText(":"+MW);
+            tv3.setText(send);
         }
 
-        Button button=(Button)findViewById(R.id.button2);
+        Button button=(Button)findViewById(R.id.button1);
         button.setOnClickListener(new View.OnClickListener(){
 
             @Override
