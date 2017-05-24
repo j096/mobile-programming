4103d1b483b245923dee7e4d760a81a3d31155f0
j096 <ji096@hanmail.net>
2017-05-25 02:22:04 +0900

    LAB6

--- app/src/main/java/org/androidtown/lab/MainActivity.java
+++ app/src/main/java/org/androidtown/lab/MainActivity.java
@@ -1,79 +1,132 @@
 package org.androidtown.lab;
 
-
-import android.content.Context;
-import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
-import android.app.Activity;
-import android.graphics.Color;
-import android.view.ContextMenu;
-import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.RadioButton;
-import android.widget.RadioGroup;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 
 public class MainActivity extends AppCompatActivity {
 
-
-    Button button1;
-    EditText ed;
-    RadioGroup radGroupMW;
-    RadioButton M;
-    RadioButton W;
-    CheckBox cb1;
-    CheckBox cb2;
-
+    Button b1;
+    Button b2;
+    Button b3;
+    Button b4;
+    EditText et;
+    String text;
+    String note="text.txt";
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button1=(Button)findViewById(R.id.button1);
-        ed=(EditText)findViewById(R.id.nameline);
-        radGroupMW=(RadioGroup)findViewById(R.id.radGroupMW);
-        M=(RadioButton) findViewById(R.id.M);
-        W=(RadioButton)findViewById(R.id.W);
-        cb1=(CheckBox)findViewById(R.id.sms);
-        cb2=(CheckBox)findViewById(R.id.email);
+        et=(EditText)findViewById(R.id.editText);
+        b1=(Button)findViewById(R.id.button);
+        b2=(Button)findViewById(R.id.button2);
+        b3=(Button)findViewById(R.id.button3);
+        b4=(Button)findViewById(R.id.button4);
 
 
-        button1.setOnClickListener(new View.OnClickListener(){
+
+        b1.setOnClickListener(new View.OnClickListener(){
             @Override
-            public void onClick(View view) {
-                Intent i=new Intent(getApplicationContext(),NewActivity.class);
+            public void onClick(View v){
 
-                String name=ed.getText().toString();
-                String check = ":";
-                int radioId=radGroupMW.getCheckedRadioButtonId();
 
-                if(M.getId()==radioId)
-                    i.putExtra("MW","남");
+                text=et.getText().toString();
+                File sd= Environment.getExternalStorageDirectory();
+                File directory=new File(sd.getAbsolutePath()+"/MyFiles");
 
-                if(W.getId()==radioId)
-                    i.putExtra("MW","여");
-
-                if(cb1.isChecked()) {
-                    check += "SMS ";
-                    i.putExtra("check", check);
+                directory.mkdirs();
+                File file=new File(directory+"/"+note);
+                try {
+                    FileOutputStream fOut=new FileOutputStream(file);
+                    OutputStreamWriter osw=new OutputStreamWriter(fOut);
+                    osw.write(text);
+                    osw.close();
+                    fOut.close();
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
                 }
-                if(cb2.isChecked()) {
-                    check+="e-mail";
-                    i.putExtra("check", check);
-                }
 
-                i.putExtra("name",name);
-                startActivity(i);
 
             }
         });
+
+        b2.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                et.setText("");
+            }
+        });
+
+        b3.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                File sd=Environment.getExternalStorageDirectory();
+                File directory=new File(sd.getAbsolutePath()+"/MyFiles");
+
+                directory.mkdirs();
+                File file=new File(directory,note);
+                try {
+                    FileInputStream fIn = new FileInputStream(file);
+                    InputStreamReader isr=new InputStreamReader(fIn);
+
+
+
+                    if(fIn!=null){
+                        BufferedReader reader=new BufferedReader(isr);
+                        String str="";
+                        StringBuffer buf=new StringBuffer();
+
+                        while((str=reader.readLine())!=null){
+                            buf.append(str+"\n");
+                        }
+                        et.setText(buf.toString());
+
+                        isr.close();
+                        fIn.close();
+
+                    }
+
+                } catch (FileNotFoundException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+
+            }
+        });
+
+
+        b4.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v){
+                finish();
+            }
+        });
+
+
     }
 
-
-
 }
+
+
+
+
+
+
